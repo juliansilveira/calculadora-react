84e7d6fa72946e790d3dd0c584859237ab276bcd
juliansilveira <julian.silveira.annoni@gmail.com>
2023-10-25 10:46:38 -0300

    modificados:     public/index.html 	modificados:     src/App.css 	modificados:     src/App.js 	nuevos archivos: src/components/Boton.js 	nuevos archivos: src/components/Calculadora.js 	nuevos archivos: src/components/boton.css   nuevos archivos: src/components/calculadora.css

---
+++ src/components/Boton.js
@@ -0,0 +1,13 @@
+import './boton.css'
+
+const Boton = (params) => {
+  const {texto, clase, handleClick} = params
+
+  return (
+    <>
+      <button className={clase} onClick={handleClick}>{texto}</button>
+    </>
+  )
+}
+
+export default Boton;
